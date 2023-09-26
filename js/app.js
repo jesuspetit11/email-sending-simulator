02fa99a8d9af0abb06bc211004a5e406000ef87e
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-09-26 18:06:15 -0400

    starting project

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//Para ejecutar después que todo el código HTML haya sido descargado
+document.addEventListener("DOMContentLoaded", function() {
+    //Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector("#email");
+    const inputAsunto = document.querySelector("#asunto");
+    const inputMensaje = document.querySelector("#mensaje");
+    console.log(inputEmail); //Para verificar si se esta seleccionando adecuadamente
+    console.log(inputAsunto); //Para verificar si se esta seleccionando adecuadamente
+
+});

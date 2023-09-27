90b7052420dd4d59fea31d997299101079471b8b
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-09-27 19:39:18 -0400

    project 1

--- js/app.js
+++ js/app.js
@@ -7,4 +7,15 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log(inputEmail); //Para verificar si se esta seleccionando adecuadamente
     console.log(inputAsunto); //Para verificar si se esta seleccionando adecuadamente
 
+    inputEmail.addEventListener("blur", validar);
+    inputAsunto.addEventListener("blur", validar);
+    inputAsunto.addEventListener("blur", validar);
+
+    function validar() {
+        if (e.target.value === ""){
+            console.log("Esta vacio");
+        } else {
+            console.log("Si hay algo")
+        }
+    }
 });

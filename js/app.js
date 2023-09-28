7acce3e08b68fbbf9a7068e2b60cf9ad81e9402d
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-09-28 04:42:47 -0400

    testing js

--- js/app.js
+++ js/app.js
@@ -4,18 +4,30 @@ document.addEventListener("DOMContentLoaded", function() {
     const inputEmail = document.querySelector("#email");
     const inputAsunto = document.querySelector("#asunto");
     const inputMensaje = document.querySelector("#mensaje");
+    const formulario = document.querySelector("#formulario");
     console.log(inputEmail); //Para verificar si se esta seleccionando adecuadamente
     console.log(inputAsunto); //Para verificar si se esta seleccionando adecuadamente
 
     inputEmail.addEventListener("blur", validar);
     inputAsunto.addEventListener("blur", validar);
-    inputAsunto.addEventListener("blur", validar);
+    inputMensaje.addEventListener("blur", validar);
 
-    function validar() {
-        if (e.target.value === ""){
-            console.log("Esta vacio");
+    function validar(e) {
+        console.log(e.target.id);
+        if (e.target.value.trim() === ""){
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`);
         } else {
-            console.log("Si hay algo")
+            //Generar alert
+            
         }
     }
 });
+
+function mostrarAlerta(mensaje) {
+            const error = document.createElement("p");
+            error.textContent = mensaje;
+            
+            //Inyectar el error al formulario
+            formulario.appendChild(error);
+            // formulario.innerHTML = error.innerHTML;
+}
